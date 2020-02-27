d179a1cea385a3d9d30a7e3d5ff0d18035fba95b
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-02-26 21:22:50 -0300

    Botões para Adicionar Transação

--- lib/app.dart
+++ lib/app.dart
@@ -5,26 +5,33 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Despesas Pessoais"),
-          backgroundColor: Colors.purple,
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                // width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  elevation: 5,
-                  child: Text('Grafico'),
-                ),
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              // width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Grafico'),
               ),
-              TransactionUser()
-            ],
-          ),
-        ));
+            ),
+            TransactionUser()
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
