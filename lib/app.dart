69146a0261110bbb12df973ca2d095f04bd9e400
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-03-10 15:10:30 -0300

    Calculando Tamanho Dinamicamente

--- lib/app.dart
+++ lib/app.dart
@@ -47,27 +47,37 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text("Despesas Pessoais"),
+      //backgroundColor: Theme.of(context).primaryColor,
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactionFormModal(context))
+      ],
+    );
+    final availableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Despesas Pessoais"),
-        //backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => _openTransactionFormModal(context))
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(
-              recentTransaction: _recenteTransactions,
+            Container(
+              height: availableHeight * 0.3,
+              child: Chart(
+                recentTransaction: _recenteTransactions,
+              ),
             ),
-            TransactionList(
-              transactions: _transactions,
-              removerTransction: _removerTransaction,
+            Container(
+              height: availableHeight * 0.7,
+              child: TransactionList(
+                transactions: _transactions,
+                removerTransction: _removerTransaction,
+              ),
             ),
           ],
         ),
--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,65 +9,62 @@ class TransactionList extends StatelessWidget {
       {@required this.transactions, @required this.removerTransction});
 
   @override
-  Widget build(BuildContext context) => Container(
-        height: 400,
-        child: transactions.isEmpty
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    "Nenhuma Transação Cadastrada",
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: 200,
-                    child: Image.asset(
-                      "assets/images/waiting.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              )
-            : ListView.builder(
-                itemCount: transactions.length,
-                itemBuilder: (ctx, index) {
-                  final tr = transactions[index];
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(
-                      vertical: 8,
-                      horizontal: 5,
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: FittedBox(child: Text('${tr.valueGet}')),
-                        ),
-                      ),
-                      title: Text(
-                        tr.title,
-                        style: Theme.of(context).textTheme.title,
-                      ),
-                      subtitle: Text(tr.dateGet),
-                      trailing: IconButton(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Colors.redAccent,
-                          ),
-                          onPressed: () {
-                            removerTransction(tr.id);
-                          }),
-                    ),
-                  );
-                },
+  Widget build(BuildContext context) => transactions.isEmpty
+      ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Nenhuma Transação Cadastrada",
+              style: Theme.of(context).textTheme.title,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 200,
+              child: Image.asset(
+                "assets/images/waiting.png",
+                fit: BoxFit.cover,
               ),
-      );
+            ),
+          ],
+        )
+      : ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            final tr = transactions[index];
+            return Card(
+              elevation: 5,
+              margin: EdgeInsets.symmetric(
+                vertical: 8,
+                horizontal: 5,
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FittedBox(child: Text('${tr.valueGet}')),
+                  ),
+                ),
+                title: Text(
+                  tr.title,
+                  style: Theme.of(context).textTheme.title,
+                ),
+                subtitle: Text(tr.dateGet),
+                trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.redAccent,
+                    ),
+                    onPressed: () {
+                      removerTransction(tr.id);
+                    }),
+              ),
+            );
+          },
+        );
 }
