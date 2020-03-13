e44a1bccff2f6d3da7e4357c7fa1baa56f0b65c2
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-03-12 21:24:20 -0300

    Ajustando Formulário

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -45,59 +45,65 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onSubmitted: (_) => _submitForm(),
-              keyboardType: TextInputType.text,
-              controller: _titleController,
-              decoration: InputDecoration(labelText: "Titulo"),
-            ),
-            TextField(
-              onSubmitted: (_) => _submitForm(),
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              controller: _valueController,
-              decoration: InputDecoration(labelText: "Valor (R\$)"),
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? 'Nenhuma data selecionada!'
-                          : "Data selecionada: ${DateFormat("d/MM/y").format(this._selectedDate).toString()}",
-                    ),
-                  ),
-                  FlatButton(
-                      textColor: Theme.of(context).primaryColor,
-                      onPressed: _showDatePicker,
-                      child: Text(
-                        "Selecionar Data",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ))
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(
+              top: 10,
+              right: 10,
+              left: 10,
+              bottom: 10 + MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                onSubmitted: (_) => _submitForm(),
+                keyboardType: TextInputType.text,
+                controller: _titleController,
+                decoration: InputDecoration(labelText: "Titulo"),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                FlatButton(
-                    //hoverColor: Colors.purpleAccent,
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).textTheme.button.color,
-                    onPressed: _submitForm,
-                    child: Text(
-                      "Nova Transação",
-                    )),
-              ],
-            )
-          ],
+              TextField(
+                onSubmitted: (_) => _submitForm(),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                controller: _valueController,
+                decoration: InputDecoration(labelText: "Valor (R\$)"),
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? 'Nenhuma data selecionada!'
+                            : "Data selecionada: ${DateFormat("d/MM/y").format(this._selectedDate).toString()}",
+                      ),
+                    ),
+                    FlatButton(
+                        textColor: Theme.of(context).primaryColor,
+                        onPressed: _showDatePicker,
+                        child: Text(
+                          "Selecionar Data",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ))
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FlatButton(
+                      //hoverColor: Colors.purpleAccent,
+                      color: Theme.of(context).primaryColor,
+                      textColor: Theme.of(context).textTheme.button.color,
+                      onPressed: _submitForm,
+                      child: Text(
+                        "Nova Transação",
+                      )),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
